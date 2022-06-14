fcfb0aa97e6d3c31649121dbbc8d1b4514097b9e
Croximity <106570269+Croximity@users.noreply.github.com>
2022-06-13 21:47:24 -0600

    Add files via upload

---
+++ Ejercicio432.java
@@ -0,0 +1,14 @@
+//Ejercicio 4.32
+
+public class Ejercicio432 {
+    public static void main(String[] args) {
+        String linea[] = new String[4];
+
+        System.out.println(" *tablero* ");
+
+        for (int i = 0; i < linea.length; i++) {
+            System.out.println("* * * * * * * *");
+            System.out.println(" * * * * * * * *");
+        }
+    }
+}
