cde4ca48e6f52caab56ad48d7774008664fb5824
Croximity <106570269+Croximity@users.noreply.github.com>
2022-06-13 22:25:43 -0600

    Add files via upload

---
+++ Seccion5/Ejercicio524.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio524 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int k = n - i; k > 0; k--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int k = 0; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = n - i - 1; k > 0; k--) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+}
